3fc0326385dfc1d638bb801fd1b25fd1f7c25853
Will Scott <willscott@gmail.com>
2014-10-26 14:41:33 -0400

    initial implementation

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ freedomjs-social-publiclog
 ==========================
 
 A freedom.js social provider backed by a public writable log
+
+The log used is passed in to the login function.
+An example of a valid implementation of a public log implementing the expected get and append
+interface is at
+
+https://script.google.com/d/1RHD35GMcgVlzd3vw4rlb0Sgfegmw9ksKoUriL8V72eo3Uh1vuDe4wV50/edit?usp=sharing
+
+The features expected of the final published url are:
+* A get request, (optionally specifying a 'dest' query filter) returns recently appended objects.
+* A post request, specifying src, dest, and msg query parameters will append a row.
+
+In addition, the interface expects a JSONP based callback structure, which can be set in a
+callback parameter.
---
+++ index.js
@@ -0,0 +1,241 @@
+/*globals freedom */
+/*jslint indent:2, node:true, sloppy:true, browser:true */
+
+/**
+ * Implementation of a social provider backed by a public
+ * get/append log accessible via XHRs.
+ *
+ * @class LogSocialProvider
+ * @constructor
+ * @param {Function} dispatchEvent callback to signal events
+ **/
+function LogSocialProvider(dispatchEvent) {
+  this.dispatchEvent = dispatchEvent;
+
+  this.social = freedom();
+
+  this.url = null;
+  this.id = null;     // userId of this user
+  this.user = Math.random();
+  
+  this.users = {};    // List of seen users (<user_profile>)
+  this.clients = {};  // List of seen clients (<client_state>)
+}
+
+/**
+ * Connect to the log.
+ * e.g. social.login(Object options)
+ * The login options used are:
+ * 'agent' - The destination / filter employed
+ * 'url' - The log used
+ *
+ * @method login
+ * @param {Object} loginOptions
+ * @return {Object} status - Same schema as 'onStatus' events
+ **/
+LogSocialProvider.prototype.login = function (loginOpts, continuation) {
+  // Wrap the continuation so that it will only be called once by
+  // onmessage in the case of success.
+
+  if (this.url !== null) {
+    continuation(undefined, this.err("LOGIN_ALREADYONLINE"));
+    return;
+  }
+  this.url = loginOpts.url;
+  this.agent = loginOpts.agent;
+  this.beginMonitoring();
+  this.write('ONLINE', continuation);
+};
+
+
+
+/**
+ * Returns all the <user_profile>s that we've seen so far (from 'onUserProfile' events)
+ * Note: the user's own <user_profile> will be somewhere in this list. 
+ * Use the userId returned from social.login() to extract your element
+ * NOTE: This does not guarantee to be entire roster, just users we're currently aware of at the moment
+ * e.g. social.getUsers();
+ *
+ * @method getUsers
+ * @return {Object} { 
+ *    'userId1': <user_profile>,
+ *    'userId2': <user_profile>,
+ *     ...
+ * } List of <user_profile>s indexed by userId
+ *   On failure, rejects with an error code (see above)
+ **/
+LogSocialProvider.prototype.getUsers = function (continuation) {
+  if (this.url === null) {
+    continuation(undefined, this.err("OFFLINE"));
+    return;
+  }
+  continuation(this.users);
+};
+
+/**
+ * Returns all the <client_state>s that we've seen so far (from any 'onClientState' event)
+ * Note: this instance's own <client_state> will be somewhere in this list
+ * Use the clientId returned from social.login() to extract your element
+ * NOTE: This does not guarantee to be entire roster, just clients we're currently aware of at the moment
+ * e.g. social.getClients()
+ * 
+ * @method getClients
+ * @return {Object} { 
+ *    'clientId1': <client_state>,
+ *    'clientId2': <client_state>,
+ *     ...
+ * } List of <client_state>s indexed by clientId
+ *   On failure, rejects with an error code (see above)
+ **/
+LogSocialProvider.prototype.getClients = function (continuation) {
+  if (this.url === null) {
+    continuation(undefined, this.err("OFFLINE"));
+    return;
+  }
+  continuation(this.clients);
+};
+
+/** 
+ * Send a message to user on your network
+ * If the destination is not specified or invalid, the message is dropped
+ * Note: userId and clientId are the same for this.websocket
+ * e.g. sendMessage(String destination_id, String message)
+ * 
+ * @method sendMessage
+ * @param {String} destination_id - target
+ * @return nothing
+ **/
+LogSocialProvider.prototype.sendMessage = function (to, msg, continuation) {
+  if (this.url === null) {
+    continuation(undefined, this.err("OFFLINE"));
+    return;
+  } else if (!this.clients.hasOwnProperty(to) && !this.users.hasOwnProperty(to)) {
+    continuation(undefined, this.err("SEND_INVALIDDESTINATION"));
+    return;
+  }
+
+  //post
+  this.write(JSON.stringify({to: to, msg: msg}), continuation);
+};
+
+/**
+   * Disconnects from the Web Socket server
+   * e.g. logout(Object options)
+   * No options needed
+   * 
+   * @method logout
+   * @return {Object} status - same schema as 'onStatus' events
+   **/
+LogSocialProvider.prototype.logout = function (continuation) {
+  if (this.url === null) { // We may not have been logged in
+    this.changeRoster(this.id, false);
+    continuation(undefined, this.err("OFFLINE"));
+    return;
+  }
+  this.url = null;
+  continuation();
+  this.changeRoster(this.id, false);
+};
+
+/**
+ * INTERNAL METHODS
+ **/
+
+var onRead;
+
+LogSocialProvider.prototype.beginMonitoring = function () {
+  if (!this.url) {
+    return;
+  }
+
+  onRead = function (resp) {
+    if (resp.items) {
+      resp.items.forEach(function (item) {
+        this.changeRoster(item.from, true);
+        if (item.msg.indexOf(this.user) > 0) {
+          this.dispatchEvent('onMessage', {
+            from: item.from,
+            message: JSON.parse(item.msg).msg
+          });
+        }
+      }.bind(this));
+    }
+    setTimeout(this.beginMonitoring.bind(this), 1000);
+  }.bind(this);
+  
+  // TODO: use core API for jsonp.
+  importScripts(this.url + "?callback=onRead&dest=" + this.agent);
+};
+
+var onWrite;
+
+LogSocialProvider.prototype.write = function (msg, cb) {
+  onWrite = cb;
+  
+  importScripts(this.url + "?callback=onWrite&dest=" + this.agent + "&from=" + this.user + "&msg=" + msg);
+};
+
+/**
+ * Dispatch an 'onClientState' event with the following status and return the <client_card>
+ * Modify entries in this.users and this.clients if necessary
+ * Note, because this provider has a global buddylist of ephemeral clients, we trim all OFFLINE users
+ *
+ * @method changeRoster
+ * @private
+ * @param {String} id - userId and clientId are the same in this provider
+ * @param {Boolean} stat - true if "ONLINE", false if "OFFLINE".
+ *                          "ONLINE_WITH_OTHER_APP"
+ * @return {Object} - same schema as 'onStatus' event
+ **/
+LogSocialProvider.prototype.changeRoster = function (id, stat) {
+  var newStatus, result = {
+    userId: id,
+    clientId: id,
+    lastUpdated: (this.clients.hasOwnProperty(id)) ? this.clients[id].lastUpdated : (new Date()).getTime(),
+    lastSeen: (new Date()).getTime()
+  };
+  if (stat) {
+    newStatus = "ONLINE";
+  } else {
+    newStatus = "OFFLINE";
+  }
+  result.status = newStatus;
+  if (!this.clients.hasOwnProperty(id) ||
+      (this.clients[id] && this.clients[id].status !== newStatus)) {
+    this.dispatchEvent('onClientState', result);
+  }
+
+  if (stat) {
+    this.clients[id] = result;
+    if (!this.users.hasOwnProperty(id)) {
+      this.users[id] = {
+        userId: id,
+        name: id,
+        lastUpdated: (new Date()).getTime()
+      };
+      this.dispatchEvent('onUserProfile', this.users[id]);
+    }
+  } else {
+    delete this.users[id];
+    delete this.clients[id];
+  }
+  return result;
+};
+
+LogSocialProvider.prototype.err = function (code) {
+  var err = {
+    errcode: code,
+    message: this.social.ERRCODE[code]
+  };
+  return err;
+};
+
+/** REGISTER PROVIDER **/
+if (typeof freedom !== 'undefined') {
+  freedom().provideAsynchronous(LogSocialProvider);
+}
+
+if (typeof exports !== 'undefined') {
+  exports.provider = LogSocialProvider;
+  exports.name = 'social';
+}
